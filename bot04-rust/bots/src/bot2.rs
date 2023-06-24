9a1ca23df4df18346893d6c7ca3aaa15b06aceb0
Eduard Luhtonen <luhtonen@gmail.com>
2023-06-24 12:30:33 +0200

    Live location Telegram bot with Rust

---
+++ bot04-rust/bots/src/bot2.rs
@@ -0,0 +1,20 @@
+use teloxide::Bot;
+use teloxide::prelude::*;
+
+// need to set TELOXIDE_TOKEN environment variable with Telegram token
+#[tokio::main]
+async fn main() {
+    let bot = Bot::from_env();
+
+    let locations = [
+        (35.652832, 139.839478),
+        (35.652832, 138.839478),
+        (35.652832, 137.839478),
+        (35.652832, 136.839478),
+    ];
+    // TODO: iterate over multiple locations
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_location(msg.chat.id, 35.652832, 139.839478).live_period(60).await?;
+        Ok(())
+    }).await;
+}
