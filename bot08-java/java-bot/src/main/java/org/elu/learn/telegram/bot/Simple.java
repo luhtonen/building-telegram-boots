81ed6c66a8e750e8afdc4a06ced6209eab703b34
Eduard Luhtonen <luhtonen@gmail.com>
2023-06-28 13:42:28 +0200

    sending photo

--- bot08-java/java-bot/src/main/java/org/elu/learn/telegram/bot/Simple.java
+++ bot08-java/java-bot/src/main/java/org/elu/learn/telegram/bot/Simple.java
@@ -4,6 +4,9 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.request.SendPhoto;
+
+import java.io.File;
 
 public class Simple {
     public static void main(String[] args) {
@@ -17,6 +20,13 @@ public class Simple {
                 final var chatId = updates.get(0).message().chat().id();
                 final var requestText = new SendMessage(chatId, "*hello from java*").parseMode(ParseMode.MarkdownV2);
                 bot.execute(requestText);
+
+                try {
+                    final var requestPhoto = new SendPhoto(chatId, new File("resources/dog.jpg"));
+                    bot.execute(requestPhoto);
+                } catch (Exception e) {
+                    System.out.println("exception caught: " + e.getLocalizedMessage());
+                }
             }
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
         });
